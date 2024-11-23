11194b241c7faed507a3e84de294ace5d790e22d
Dora Lee <dsa28s@naver.com>
2024-11-23 23:29:12 +0900

    fix: error when user not found, user signed out

---
+++ src/app/(auth)/sign-in/layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { getCurrentUser } from '@/lib/db/queries';
+import { createServerSupabase } from '@/lib/db/supabase/server';
+import { redirect, RedirectType } from 'next/navigation';
+
+export default async function SignInLayout({ children }: React.PropsWithChildren) {
+  const supabase = await createServerSupabase();
+  try {
+    await getCurrentUser(supabase);
+  } catch (e) {
+    return <>{children}</>;
+  }
+
+  redirect('/', RedirectType.replace);
+}
