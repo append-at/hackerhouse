bbbfbada03db9f8b0ef57219879d63d21c7801fd
Chanhee Lee <hiddenest@naver.com>
2024-11-24 05:22:12 +0900

    feat: show profile in the main setting page

--- src/app/(service)/setting/page.tsx
+++ src/app/(service)/setting/page.tsx
@@ -6,8 +6,12 @@ import Link from 'next/link';
 import { Header } from '../_layouts/header';
 import { signOut } from './actions';
 import SubscribePushNotificationButton from './subscribe-notification';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { useUser } from '@/app/context';
 
 const Page = () => {
+  const user = useUser();
+
   const handleClickSignOut = async () => {
     signOut();
   };
@@ -16,7 +20,15 @@ const Page = () => {
     <>
       <Header title='Setting' />
 
-      <div className='space-y-2'>
+      <div className='flex items-center gap-3 border-b border-solid border-border px-6 py-6'>
+        <Avatar className='size-12'>
+          <AvatarImage src={user.avatar_url ?? ''} />
+          <AvatarFallback>{user.name?.[0].toUpperCase()}</AvatarFallback>
+        </Avatar>
+        <p className='text-lg font-medium'>{user.name}</p>
+      </div>
+
+      <div className='space-y-2 py-3'>
         <Button
           className='w-full justify-start px-6 text-base'
           variant='ghost'
@@ -25,7 +37,7 @@ const Page = () => {
         >
           <Link href='/setting/profile'>
             <UserIcon />
-            <span>Profile</span>
+            <span>Edit Profile</span>
           </Link>
         </Button>
         <SubscribePushNotificationButton />
