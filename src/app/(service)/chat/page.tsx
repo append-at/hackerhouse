9f0a8d49fed88a656934fac6eac087f59a4db0b8
Dora Lee <dsa28s@naver.com>
2024-11-24 01:56:01 +0900

    fix(chat): prevent create multiple conversation via same content

--- src/app/(service)/chat/page.tsx
+++ src/app/(service)/chat/page.tsx
@@ -1,20 +1,21 @@
 import { getCurrentUser, listMyAiConversations } from '@/lib/db/queries';
 import { createServerSupabase } from '@/lib/db/supabase/server';
 import ChatInterface from './_page';
-import { generateId } from '@/lib/utils';
+import { generateId } from 'ai';
 
 const Page = async () => {
   const supabase = await createServerSupabase();
   const user = await getCurrentUser(supabase);
   const conversations = await listMyAiConversations(supabase);
 
-  const sessionId = crypto.randomUUID();
+  let sessionId: string = '';
   const initialMessages = conversations.flatMap((conversations) => conversations.data);
 
   // if user first message
-  if (initialMessages.length === 0) {
+  if (conversations.length === 0) {
+    sessionId = crypto.randomUUID();
     const initialMessage = {
-      id: generateId(16),
+      id: generateId(),
       role: 'assistant',
       content: `Hey ${user.name.split(' ')[0]}! How's it going?`,
       createdAt: new Date().toISOString(),
@@ -26,6 +27,8 @@ const Page = async () => {
     });
 
     initialMessages.push(initialMessage);
+  } else {
+    sessionId = conversations[conversations.length - 1].id;
   }
 
   return (
