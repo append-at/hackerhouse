a5b61846ff81d9e5289dfa7309c45bab25d6a6c8
Dora Lee <dsa28s@naver.com>
2024-11-23 15:14:56 +0900

    feat: add base chat api (without prompt)

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,13 +1,13 @@
-import { openai } from "@ai-sdk/openai";
-import { streamText } from "ai";
+import { openai } from '@ai-sdk/openai';
+import { streamText } from 'ai';
 
-export const runtime = "edge";
+export const runtime = 'edge';
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
   const result = streamText({
-    model: openai("gpt-4o"),
+    model: openai('gpt-4o'),
     messages,
   });
 
